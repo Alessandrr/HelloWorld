710cb2d913bbbbb9eda647711188e3b1bd546830
Aleksandr Mamlygo <alexander.mamlygo@gmail.com>
2023-04-28 19:23:55 +0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Aleksandr Mamlygo on 28.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
